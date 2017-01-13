09fbe76f6c4474afb775a80f9d47e1c99f14dd25
ChauMing <971334483@qq.com>
2017-01-14 00:45:13 +0800

    feat: icons feat

--- src/ReduiIcons.jsx
+++ src/ReduiIcons.jsx
@@ -1,27 +1,44 @@
 import React, { Component, PropTypes } from 'react'
+import classNames from 'classnames'
 import './ReduiIcons.less'
 
 export default class ReduiIcons extends Component {
   static displayName = "ReduiIcons"
 
   static propTypes = {
-    foo: PropTypes.string
+    type: PropTypes.string,
+    size: PropTypes.oneOf(['small', 'large']),
+    className: PropTypes.string
   }
 
   static defaultProps = {
-
-  }
-
-  state = {}
-
-  componentDidMount() {
-
+    type: 'success',
+    size: 'small',
+    className: ''
   }
 
   render() {
-    const { children, ...others } = this.props
+
+    const {
+      type,
+      size,
+      children,
+      className,
+      ...others
+    } = this.props
+
+    const cls = classNames(
+      'redui-icon',
+      `redui-icon-${type}`,
+      `redui-icon-${size}`,
+      className
+    )
+
     return (
-      <i className="redui-icons" {...others} >
+      <i
+        className={cls}
+        {...others}
+      >
         {children}
       </i>
     )
